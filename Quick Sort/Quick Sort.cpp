0768e9c02338c3c99b322193a9f6e5380718e456
MzSuwawa <yudharamadhan838@gmail.com>
2024-03-29 19:45:27 +0700

    Menambahkan prosedur Input

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -6,3 +6,26 @@ int arr[20];
 int cmp_count = 0; //jumlah perbandingan
 int mov_count = 0; //jumlah perpindahan data
 int n;
+
+void input() {
+	while (true)
+	{
+		cout << "Masukkan panjang element array :";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
+
+	cout << "\n-------------------" << endl;
+	cout << "\nEnter array Element" << endl;
+	cout << "\n-------------------" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
